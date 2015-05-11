9d9dd27c01c1e5eb607d8355f051707176711f9a
ChoznJuan <djg5826@gmail.com>
2015-05-11 19:01:22 -0400

    Euphoria bring up

--- eos.mk
+++ eos.mk
@@ -21,13 +21,13 @@ TARGET_SCREEN_HEIGHT := 960
 
 $(call inherit-product, device/motorola/surnia/full_surnia.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Euphoria stuff.
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := cm_surnia
+PRODUCT_NAME := eos_surnia
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := surnia
 PRODUCT_MANUFACTURER := motorola
